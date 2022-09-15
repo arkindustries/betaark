ce63ed9cdff7c60e1b842442b412b79d399b3cc9
arkindustries <bigarkindustries@gmail.com>
2022-09-15 15:54:34 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ark from './logo/bigark.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ark} className="App-logo" alt="logo" />
+        <p>
+          Welcome to official site ARK Industries,
+          <br></br> work-in-progress.. 🚧
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
